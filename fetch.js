bf267abfa46ebe54b4ae3800904cb7dd890958c9
Ryan Meinzer <rmeinzer@copado.com>
2023-07-26 16:29:26 -0700

    basic fetch

--- fetch.js
+++ fetch.js
@@ -1,9 +1,5 @@
-const fetch = require('node-fetch');
-
 fetch('https://api.giphy.com/v1/gifs/search?q=dogs&api_key=cwxQISXRJJ5EO5nAW0kfohP1RSmK2PSE&rating=g')
   .then(response => response.json())
   .then(json =>
     console.log(json.data[0].title)
 )
-
-// console.log("Hi")
