0dfbd44bbe005553985fca8ff378679be37dc81f
Ryan Meinzer <rmeinzer@copado.com>
2023-07-27 11:21:10 -0700

    changed API Name for func

--- RyFetchDogProject/copado/functions/RyFetchDog.js
+++ RyFetchDogProject/copado/functions/RyFetchDog.js
@@ -1,5 +1,6 @@
+
 fetch('https://api.giphy.com/v1/gifs/search?q=dogs&api_key=cwxQISXRJJ5EO5nAW0kfohP1RSmK2PSE&rating=g')
-  .then(response => response.json())
-  .then(json =>
-  console.log(json.data[1].title)
+.then(response => response.json())
+.then(json =>
+console.log(json.data[1].title)
 )
