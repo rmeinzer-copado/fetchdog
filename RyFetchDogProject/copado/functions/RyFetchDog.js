45792ccd7cc5aa392bbf229f1bc2b7045dd0f4cc
Ryan Meinzer <rmeinzer@copado.com>
2023-07-27 12:17:17 -0700

    updated comment

--- RyFetchDogProject/copado/functions/RyFetchDog.js
+++ RyFetchDogProject/copado/functions/RyFetchDog.js
@@ -2,6 +2,6 @@ fetch(
   'https://api.giphy.com/v1/gifs/search?q=dogs&api_key=cwxQISXRJJ5EO5nAW0kfohP1RSmK2PSE&rating=g')
   .then(response => response.json())
   .then(json =>
-    // log third dog  
+    // log third dog's GIF title
     console.log(json.data[2].title)
   )
